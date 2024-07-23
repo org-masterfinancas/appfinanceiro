5c656d857c56873de704058913b98e7923eab811
evandrotavaresalcantara <evandrotavaresalcantara@gmail.com>
2024-07-23 18:18:02 -0300

    desativar-rotav1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,8 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/v1/lancamentoFinanceiros', rotasV1LancamentoFinanceiros);
-app.use('/v2/lancamentoFinanceiros', autenticacao, rotasV2LancamentoFinanceiros);
+app.use('/lancamentofinanceiros', autenticacao, rotasV2LancamentoFinanceiros);
 app.use('/usuarios', rotasUsuarios);
 app.use('/login', rotasLogin);
 
