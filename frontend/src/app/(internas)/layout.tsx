1a38748743c9c73e974f0ad6deaba2fade806a4c
evandrotavaresalcantara <evandrotavaresalcantara@gmail.com>
2024-08-25 14:26:33 -0300

    mantine-inicial

--- frontend/src/app/(internas)/layout.tsx
+++ frontend/src/app/(internas)/layout.tsx
@@ -1,7 +1,5 @@
 import PaginaInterna from "@/app/components/Autenticacao/PaginaInterna"
 import ThemaMantine from "../components/Mantine/ThemaMantine"
-import ThemaTailwind from "../components/Tailwind/ThemaTailwind"
-import ThemaInicial from "../components/Tailwind/ThemaInicial"
 
 export default function Layout(props: any) {
   return (
