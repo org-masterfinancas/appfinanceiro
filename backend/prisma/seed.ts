dfd62c1f4b6250a779ecb3a432ccffd76acabe6f
evandrotavaresalcantara <evandrotavaresalcantara@gmail.com>
2024-10-30 18:38:28 -0300

    seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,36 +1,35 @@
 import { PrismaClient } from '@prisma/client'
-import id from '../src/shared/id'
-import { StatusLancamento } from '../src/model/enum/status-lancamento'
-import { TipoLancamento } from '../src/model/enum/tipo-lancamento'
-import Senha from '../src/shared/senha'
 
 const prisma = new PrismaClient()
 
 async function main() {
   const usuarios = [
-    { email: 'evandro@webapp.dev.br', senha: 'e123', nome: 'evandro', sobrenome: 'sobrenome', descricaoLancamento: 'aws', valorLancamento: 1850 },
-    { email: 'dev@webapp.dev.br', senha: 'd123', nome: 'dev', sobrenome: 'sobrenome', descricaoLancamento: 'dev', valorLancamento: 3000 },
-    { email: 'visitante@webapp.dev.br', senha: 'v123', nome: 'Visitante', sobrenome: 'sobrenome', descricaoLancamento: 'dev', valorLancamento: 3000 }
+    { id: '4950cb39-c335-4365-880b-7081fc8c87a0', email: 'evandro@webapp.dev.br', senha: '$2b$05$l/ESacSyfV7KYA5Ybt/w4elOrF3DyduSknpt6wAbL9f/.2oe1ULgq', nome: 'evandro', sobrenome: 'sobrenome', descricaoLancamento: 'aws', valorLancamento: 1850 },
+    { id: '0d58d580-3fc2-40c8-942f-fd4825cbd550', email: 'dev@webapp.dev.br', senha: '$2b$05$mOqQu7rWbotvBBgQdcpZeepejI3Zf8W5lz7bTl7UAfjzaO8bQPalm', nome: 'dev', sobrenome: 'sobrenome', descricaoLancamento: 'dev', valorLancamento: 3000 },
+    { id: '7ae78fc9-fda4-4c1d-8cfb-325f2b0e4489', email: 'visitante@webapp.dev.br', senha: '$2b$05$ZMTze5WasX4N2qGvaqk5y.LJaRxoq5JW.g6YXm3xR3KHzT31hSah6', nome: 'Visitante', sobrenome: 'sobrenome', descricaoLancamento: 'dev', valorLancamento: 3000 }
   ]
+
+  await prisma.lancamentoFinanceiros.deleteMany()
+  await prisma.usuarios.deleteMany()
 
   for (const usuario of usuarios) {
     await prisma.usuarios.upsert({
       where: { email: usuario.email },
       update: {},
       create: {
-        id: id.novo(),
+        id: usuario.id,
         email: usuario.email,
-        senha: await Senha.criptografar(usuario.senha),
+        senha: usuario.senha,
         nome: usuario.nome,
         sobrenome: usuario.sobrenome,
         lancamentoFinanceiros: {
           create: {
-            id: id.novo(),
+            id: usuario.id,
             descricaoLancamento: usuario.descricaoLancamento,
-            statusLancamento: StatusLancamento.CONSOLIDADO,
-            tipoLancamento: TipoLancamento.RECEITA,
+            statusLancamento: 'Pendente',
+            tipoLancamento: 'Receita',
             valorLancamento: usuario.valorLancamento,
-            dataCriacaoLancamento: new Date('2024-07-02')
+            dataCriacaoLancamento: new Date('2024-10-30')
           }
         }
       }
